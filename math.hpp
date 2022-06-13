dac4417d37c189256a5651f4d1b5ea0691404076
gelya <gelya.2004@mail.ru>
2022-06-13 21:10:04 +0200

    добавление файлов

---
+++ math.hpp
@@ -0,0 +1,396 @@
+﻿#pragma once
+using namespace std;
+class Matrix
+{
+public:
+
+    Matrix(int n, int m)
+    {
+        m_n = n;
+        m_m = m;
+        m_mat = new double* [m_n];
+        for (int i = 0; i < m_n; i++)
+        {
+            m_mat[i] = new double[m_m];
+        }
+    }
+
+
+    Matrix(const Matrix& mat)
+    {
+        m_n = mat.m_n;
+        m_m = mat.m_m;
+        m_mat = new double* [m_n];
+        for (int i = 0; i < m_n; i++)
+        {
+            m_mat[i] = new double[m_m];
+        }
+        for (int i = 0; i < m_n; i++)
+        {
+            for (int j = 0; j < m_m; j++)
+            {
+                m_mat[i][j] = mat.m_mat[i][j];
+            }
+        }
+    }
+
+    Matrix& operator=(const Matrix& mat)
+    {
+        m_n = mat.m_n;
+        m_m = mat.m_m;
+
+        for (int i = 0; i < m_n; i++)
+        {
+            for (int j = 0; j < m_m; j++)
+            {
+                m_mat[i][j] = mat.m_mat[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix operator+(const Matrix& mat)
+    {
+        Matrix tmp(m_n, mat.m_m);
+        if (m_m != mat.m_m && m_n != mat.m_n)
+        {
+            cout << "matrices have a different type " << endl;
+            return mat;
+        }
+        if (m_m != mat.m_m && m_n != mat.m_n)
+        {
+            cout << "matrices have a different type " << endl;
+            return mat;
+        }
+        else
+        {
+            for (int i = 0; i < m_n; i++)
+            {
+                for (int j = 0; j < m_m; j++)
+                {
+                    tmp.m_mat[i][j] = m_mat[i][j] + mat.m_mat[i][j];
+                }
+            }
+
+        }
+        return tmp;
+    }
+
+    Matrix operator-(const Matrix& mat)
+    {
+        Matrix tmp(m_n, mat.m_m);
+        if (m_m != mat.m_m && m_n != mat.m_n)
+        {
+            cout << "matrices have a different type " << endl;
+            return mat;
+        }
+        else
+        {
+            for (int i = 0; i < m_n; i++)
+            {
+                for (int j = 0; j < m_m; j++)
+                {
+                    tmp.m_mat[i][j] = m_mat[i][j] - mat.m_mat[i][j];
+                }
+            }
+        }
+        return tmp;
+    }
+
+    Matrix operator*(const Matrix& mat)
+    {
+        Matrix tmp(m_n, mat.m_m);
+        if (m_m != mat.m_n)
+        {
+            cout << "matrices have a different type " << endl;
+        }
+        else
+        {
+            for (int i = 0; i < m_n; i++)
+            {
+                for (int j = 0; j < mat.m_m; j++)
+                {
+                    tmp.m_mat[i][j] = 0;
+                    for (int k = 0; k < mat.m_n; k++)
+                    {
+                        tmp.m_mat[i][j] += m_mat[i][k] * mat.m_mat[k][j];
+                    }
+                }
+            }
+        }
+        return tmp;
+    }
+
+    ~Matrix()
+    {
+        for (int i = 0; i < m_n; i++)
+        {
+            delete[] m_mat[i];
+        }
+        delete m_mat;
+    }
+
+    double DET()
+    {
+        if ((m_n != m_m) && (m_n != 2) && (m_n != 3))
+        {
+            cout << "the operation is not valid " << endl;
+        }
+
+        else if (m_n == 2)
+        {
+            double det = 0;
+            det = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
+            return det;
+        }
+
+        else if (m_n == 3)
+        {
+            double det = 0;
+            det = m_mat[0][0] * m_mat[1][1] * m_mat[2][2] + m_mat[0][1] * m_mat[1][2] * m_mat[2][0]
+                + m_mat[1][0] * m_mat[2][1] * m_mat[0][2] - m_mat[2][0] * m_mat[1][1] * m_mat[0][2] -
+                m_mat[2][1] * m_mat[1][2] * m_mat[0][0] - m_mat[1][0] * m_mat[0][1] * m_mat[2][2];
+            return det;
+        }
+    }
+
+    Matrix reverse()
+    {
+        double Det = DET();
+        Matrix tmp(m_n, m_m);
+        if ((m_n == m_m && m_n == 2) || (m_n == m_m && m_n == 3))
+        {
+            if (Det == 0)
+            {
+                cout << "det = 0 " << endl;
+            }
+            else
+            {
+                if (m_n == 2)
+                {
+                    tmp.m_mat[0][0] = m_mat[1][1] / Det;
+                    tmp.m_mat[0][1] = -m_mat[0][1] / Det;
+                    tmp.m_mat[1][0] = -m_mat[1][0] / Det;
+                    tmp.m_mat[1][1] = m_mat[0][0] / Det;
+                    return tmp;
+                }
+                else if (m_n == 3)
+                {
+                    tmp.m_mat[0][0] = (m_mat[1][1] * m_mat[2][2] - m_mat[2][1] * m_mat[1][2]) / Det;
+                    tmp.m_mat[1][0] = -(m_mat[1][0] * m_mat[2][2] - m_mat[2][0] * m_mat[1][2]) / Det;
+                    tmp.m_mat[2][0] = (m_mat[1][0] * m_mat[2][1] - m_mat[2][0] * m_mat[1][1]) / Det;
+                    tmp.m_mat[0][1] = -(m_mat[0][1] * m_mat[2][2] - m_mat[2][1] * m_mat[0][2]) / Det;
+                    tmp.m_mat[1][1] = (m_mat[0][0] * m_mat[2][2] - m_mat[2][0] * m_mat[0][2]) / Det;
+                    tmp.m_mat[2][1] = -(m_mat[0][0] * m_mat[2][1] - m_mat[2][0] * m_mat[0][1]) / Det;
+                    tmp.m_mat[0][2] = (m_mat[0][1] * m_mat[1][2] - m_mat[1][1] * m_mat[0][2]) / Det;
+                    tmp.m_mat[1][2] = -(m_mat[0][0] * m_mat[1][2] - m_mat[1][0] * m_mat[0][2]) / Det;
+                    tmp.m_mat[2][2] = (m_mat[0][0] * m_mat[1][1] - m_mat[1][0] * m_mat[0][1]) / Det;
+                    return tmp;
+                }
+            }
+        }
+        else
+            cout << "the operation is not valid " << endl;
+    }
+
+    Matrix transposition()
+    {
+        swap(m_m, m_n);
+        Matrix tmp(m_n, m_m);
+        for (int i = 0; i < m_n; i++)
+        {
+            for (int j = 0; j < m_m; j++)
+            {
+                tmp.m_mat[i][j] = m_mat[j][i];
+            }
+        }
+
+        return tmp;
+
+    }
+
+    int Rang()
+    {
+        int rang = 0;
+        int k = 0;
+        int l = 0;
+        int g = 0;
+
+        double det = DET();
+
+        for (int i = 0; i < m_n; i++)
+        {
+            for (int j = 0; j < m_m; j++)
+            {
+                if (m_mat[i][j] == 0)
+                {
+                    k++;
+                }
+            }
+        }
+        if (k == m_m * m_n)
+        {
+            rang = 0;
+        }
+        else
+        {
+            if (((m_n = 1) && (m_m != 1)) || ((m_m = 1) && (m_n != 1)) || (m_n = 1) && (m_m = 1))
+            {
+                rang = 1;
+            }
+
+            if ((m_n = m_m) && (m_n == 2))
+            {
+                if (det == 0)
+                {
+                    rang = 1;
+                }
+                else
+                {
+                    rang = 2;
+                }
+            }
+
+            if ((m_n = m_m) && (m_n == 3))
+            {
+
+                g = m_mat[0][0] * m_mat[1][1] - m_mat[0][1] * m_mat[1][0];
+                if (g == 0)
+                {
+                    rang = 1;
+                }
+                else
+                {
+                    l = det;
+                    if (l == 0)
+                    {
+                        rang = 2;
+                    }
+                    else
+                    {
+                        rang = 3;
+                    }
+                }
+            }
+        }
+        return rang;
+    }
+
+    void linear_dependence()
+    {
+        double det = 0;
+        det = DET();
+        if (det == 0)
+        {
+            cout << "dependent vectors" << endl;
+        }
+        else
+        {
+            cout << "not dependent vectors" << endl;
+        }
+
+    }
+
+    Matrix operator%(const Matrix& mat)
+    {
+        const double eps = 0.00001;
+        double max = 0;
+        int index;
+        int k = 0;
+        double* x = new double[m_n];
+
+        for (int i = 0; i < m_n; i++)
+        {
+            for (int j = 0; j < m_m; j++)
+            {
+                cout << m_mat[i][j] << "*x" << j;
+                if (j < m_m - 1)
+                   cout << " + ";
+            }
+            cout << " = " << mat.m_mat[i][0] << endl;
+        }
+
+        while (k < m_n)
+        {
+            max = abs(m_mat[k][k]);
+            index = k;
+            for (int i = k + 1; i < m_n - 1; i++)
+            {
+                if (abs(m_mat[i][k]) > max)
+                {
+                    max = abs(m_mat[i][k]);
+                    index = i;
+                }
+            }
+            if (max < eps)
+            {
+                cout << "The solution cannot be obtained because of zero values " << endl;
+                return *this;
+            }
+
+            for (int j = 0; j < m_n; j++)
+            {
+                double temp = m_mat[k][j];
+                m_mat[k][j] = m_mat[index][j];
+                m_mat[index][j] = temp;
+            }
+
+            for (int i = k; i < m_n; i++)
+            {
+                double temp = m_mat[i][k];
+                if (abs(temp) < eps)
+                    continue;
+                for (int j = 0; j < m_n; j++)
+                    m_mat[i][j] = m_mat[i][j] / temp;
+                mat.m_mat[i][0] = mat.m_mat[i][0] / temp;
+                if (i == k)
+                    continue;
+                for (int j = 0; j < m_n; j++)
+                    m_mat[i][j] = m_mat[i][j] - m_mat[k][j];
+                mat.m_mat[i][0] = mat.m_mat[i][0] - mat.m_mat[k][0];
+            }
+            k++;
+        }
+        for (k = m_n - 1; k >= 0; k--)
+        {
+            x[k] = mat.m_mat[k][0];
+            for (int i = 0; i < k; i++)
+                mat.m_mat[i][0] = mat.m_mat[i][0] - m_mat[i][k] * x[k];
+        }
+        for (int i = 0; i < m_n; i++)
+            cout << "x" << i << " = " << x[i] << endl;
+        return *this;
+    }
+
+    friend istream& operator>>(istream& in, Matrix& mat);
+    friend ostream& operator<<(ostream& out, const Matrix& mat);
+private:
+    int m_n, m_m;
+    double** m_mat;
+};
+
+istream& operator>>(istream& in, Matrix& mat)
+{
+    for (int i = 0; i < mat.m_n; i++)
+    {
+        for (int j = 0; j < mat.m_m; j++)
+        {
+            in >> mat.m_mat[i][j];
+        }
+    }
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Matrix& mat)
+{
+
+    for (int i = 0; i < mat.m_n; i++)
+    {
+        for (int j = 0; j < mat.m_m; j++)
+        {
+            out << mat.m_mat[i][j] << " ";
+        }
+        out << endl;
+    }
+    return out;
+}
---
+++ progect.cpp
@@ -0,0 +1,196 @@
+﻿#include <iostream>
+#include "math.hpp"
+using namespace std;
+void Output()
+{
+	cout << "Press 0 to exit the programm" << endl;
+	cout << "Press 1 for matrix addition" << endl;
+	cout << "Press 2 for matrix subtraction" << endl;
+	cout << "Press 3 for matrix multiplication" << endl;
+	cout << "Press 4 for using the Gauss method" << endl;
+	cout << "Press 5 for checking the dependence of vectors" << endl;
+	cout << "Press 6 for finding the reverse matrix" << endl;
+	cout << "Press 7 for finding the transposition matrix" << endl;
+	cout << "Press 8 for findint the determinant for 2x2 or 3x3 type matrix" << endl;
+	cout << "Press 9 for findint the determinant for 2x2 or 3x3 type matrix" << endl;
+
+}
+
+int main()
+{
+	int p = 0;
+	int n, m;
+	Output();
+	cin >> p;
+
+	while (p != 0)
+	{
+		if ((p > 8) && (p < 0))
+		{
+			Output();
+		}
+
+		if (p == 1)
+		{
+			cout << "The number of lines and columns of matrix A: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << "The number of lines and columns of matrix B: " << endl;
+			cin >> n >> m;
+
+			Matrix B(n, m);
+			cout << "Type B matrix: " << endl;
+			cin >> B;
+
+			cout << '\n';
+			cout << A + B << endl;
+			cout << '\n';
+
+			Output();
+			cin >> p;
+
+		}
+
+		if (p == 2)
+		{
+			cout << "The number of lines and columns of matrix A: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << "The number of lines and columns of matrix B: " << endl;
+			cin >> n >> m;
+
+			Matrix B(n, m);
+			cout << "Type B matrix: " << endl;
+			cin >> B;
+
+			cout << '\n';
+			cout << A - B << endl;
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+		if (p == 3)
+		{
+			cout << "The number of lines and columns of matrix A: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << "The number of lines and columns of matrix B: " << endl;
+			cin >> n >> m;
+
+			Matrix B(n, m);
+			cout << "Type B matrix: " << endl;
+			cin >> B;
+
+			cout << '\n';
+			cout << A * B << endl;
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+		if (p == 4)
+		{
+			cout << "The number of equations: " << endl;
+			cin >> n;
+
+			Matrix A(n, n);
+			Matrix B(n, 1);
+
+			cout << "Type the coefficients: " << endl;
+			cin >> A;
+
+			cout << "Type the free terms: " << endl;
+			cin >> B;
+
+			cout << '\n';
+			A% B;
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+		if (p == 5)
+		{
+			cout << "Type three vectors: " << endl;
+
+			Matrix A(3, 3);
+			cin >> A;
+
+			cout << '\n';
+			A.linear_dependence();
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+		if (p == 6)
+		{
+			cout << "The number of lines and columns of matrix A: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << '\n';
+			cout << A.reverse();
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+		if (p == 7)
+		{
+			cout << "The number of lines and columns of matrix A: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << '\n';
+			cout << A.transposition();
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+
+
+		if (p == 9)
+		{
+			cout << "The number of lines and columns of matrix A up to matrix 3x3: " << endl;
+			cin >> n >> m;
+
+			Matrix A(n, m);
+			cout << "Type A matrix: " << endl;
+			cin >> A;
+
+			cout << '\n';
+			cout << A.Rang();
+			cout << '\n';
+
+			Output();
+			cin >> p;
+		}
+	}
+	return 0;
+}
